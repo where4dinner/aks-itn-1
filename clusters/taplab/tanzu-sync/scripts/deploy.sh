01fc9c973b2aae22b4ac40e1a43407935e8380a8
afewell <afewell@gmail.com>
2023-11-03 10:46:35 +0000

    updating reg secret

--- clusters/taplab/tanzu-sync/scripts/deploy.sh
+++ clusters/taplab/tanzu-sync/scripts/deploy.sh
@@ -5,8 +5,8 @@ set -o errexit -o nounset -o pipefail
 : "${SOPS_AGE_KEY?Expected env var to be set, but was not.}"
 
 kapp deploy -a tanzu-sync \
-  -f <(ytt -f tanzu-sync/app/config \
+  -f <(ytt -f tanzu-sync/sops/app/config \
            -f cluster-config/config/tap-install/.tanzu-managed/version.yaml \
-           --data-values-file sensitive-values.stdout=<(sops -d tanzu-sync/app/sensitive-values/tanzu-sync-values.sops.yaml) \
-           --data-values-file tanzu-sync/app/values/
+           --data-values-file sensitive-values.stdout=<(sops -d tanzu-sync/sops/app/sensitive-values/tanzu-sync-values.sops.yaml) \
+           --data-values-file tanzu-sync/sops/app/values/
       ) $@
